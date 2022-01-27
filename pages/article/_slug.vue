a0b2456d98e939aedfb541d225152fdaa05830ed
cong <458366075@qq.com>
2022-01-27 16:29:24 +0800

    done

--- pages/article/_slug.vue
+++ pages/article/_slug.vue
@@ -2,112 +2,60 @@
   <div class="article-page">
     <div class="banner">
       <div class="container">
-        <h1>How to build webapps that scale</h1>
-
-        <div class="article-meta">
-          <a href=""><img src="http://i.imgur.com/Qr71crq.jpg"></a>
-          <div class="info">
-            <a href="" class="author">Eric Simons</a>
-            <span class="date">January 20th</span>
-          </div>
-          <button class="btn btn-sm btn-outline-secondary">
-            <i class="ion-plus-round" />
-            &nbsp;
-            Follow Eric Simons <span class="counter">(10)</span>
-          </button>
-                &nbsp;&nbsp;
-          <button class="btn btn-sm btn-outline-primary">
-            <i class="ion-heart" />
-            &nbsp;
-            Favorite Post <span class="counter">(29)</span>
-          </button>
-        </div>
+        <h1>{{ article.title }}</h1>
+        <author-panel :article="article" @changeFollowing="changeFollowing" @changeArticle="changeArticle" />
       </div>
     </div>
 
     <div class="container page">
       <div class="row article-content">
         <div class="col-md-12">
-          <p>
-            Web development technologies have evolved at an incredible clip over the past few years.
-          </p>
-          <h2 id="introducing-ionic">
-            Introducing RealWorld.
-          </h2>
-          <p>It's a great solution for learning how other frameworks work.</p>
+          <div v-html="article.body" />
+          <ul class="tag-list">
+            <li v-for="item in article.tagList" :key="item" class="tag-default tag-pill tag-outline">
+              {{ item }}
+            </li>
+          </ul>
         </div>
       </div>
 
       <hr>
 
-      <div class="article-actions">
-        <div class="article-meta">
-          <a href="profile.html"><img src="http://i.imgur.com/Qr71crq.jpg"></a>
-          <div class="info">
-            <a href="" class="author">Eric Simons</a>
-            <span class="date">January 20th</span>
-          </div>
-
-          <button class="btn btn-sm btn-outline-secondary">
-            <i class="ion-plus-round" />
-            &nbsp;
-            Follow Eric Simons
-          </button>
-                &nbsp;
-          <button class="btn btn-sm btn-outline-primary">
-            <i class="ion-heart" />
-            &nbsp;
-            Favorite Post <span class="counter">(29)</span>
-          </button>
-        </div>
+      <div v-if="article.author" class="article-actions">
+        <author-panel :article="article" />
       </div>
 
       <div class="row">
         <div class="col-xs-12 col-md-8 offset-md-2">
           <form class="card comment-form">
             <div class="card-block">
-              <textarea class="form-control" placeholder="Write a comment..." rows="3" />
+              <textarea v-model="body" v-loading="loading" class="form-control" placeholder="Write a comment..." rows="3" />
             </div>
             <div class="card-footer">
-              <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img">
-              <button class="btn btn-sm btn-primary">
+              <img v-if="article.author" :src="article.author.image" class="comment-author-img">
+              <button v-loading="loading" class="btn btn-sm btn-primary" @click="createComment">
                 Post Comment
               </button>
             </div>
           </form>
 
-          <div class="card">
+          <div v-for="item in comments" :key="item.id" class="card">
             <div class="card-block">
               <p class="card-text">
-                With supporting text below as a natural lead-in to additional content.
+                {{ item.body }}
               </p>
             </div>
             <div class="card-footer">
-              <a href="" class="comment-author">
-                <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img">
-              </a>
-                        &nbsp;
-              <a href="" class="comment-author">Jacob Schmidt</a>
-              <span class="date-posted">Dec 29th</span>
-            </div>
-          </div>
-
-          <div class="card">
-            <div class="card-block">
-              <p class="card-text">
-                With supporting text below as a natural lead-in to additional content.
-              </p>
-            </div>
-            <div class="card-footer">
-              <a href="" class="comment-author">
-                <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img">
-              </a>
-                        &nbsp;
-              <a href="" class="comment-author">Jacob Schmidt</a>
-              <span class="date-posted">Dec 29th</span>
+              <n-link :to="`/profile/${article.author.username}`" class="comment-author">
+                <img :src="article.author.image" class="comment-author-img">
+              </n-link>
+              &nbsp;
+              <n-link :to="`/profile/${article.author.username}`" class="comment-author">
+                {{ article.author.username }}
+              </n-link>
+              <span class="date-posted">{{ article.updatedAt | format }}</span>
               <span class="mod-options">
-                <i class="ion-edit" />
-                <i class="ion-trash-a" />
+                <i v-loading="loading" class="ion-trash-a" @click="deleteComment(item.id)" />
               </span>
             </div>
           </div>
@@ -117,10 +65,72 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'PageArticle'
+<script lang="ts">
+import Vue from 'vue'
+import { articlesSlugApi, articlesSlugCommentsApi, articlesSlugCommentsCreateApi, articlesSlugCommentsDeleteApi } from '@/api/article'
+import { Article, Comment } from '@/api/article.d'
+import AuthorPanel from '@/components/PanelAuthor.vue'
+
+interface Data {
+  article?: Article
+  body: string
+  loading: boolean,
+  comments?: Comment[]
 }
+
+export default Vue.extend({
+  name: 'PageArticle',
+  components: {
+    AuthorPanel
+  },
+  async asyncData ({ params: { slug } }) {
+    const { article } = await articlesSlugApi(slug)
+    const { comments } = await articlesSlugCommentsApi(slug)
+    return { article, comments }
+  },
+  data (): Data {
+    return {
+      body: '',
+      loading: false,
+    }
+  },
+  head () {
+    return {
+      title: `${this.$route.params.slug.split('-')[0]} - Conduit`
+    }
+  },
+  computed: {
+    slug (): string {
+      return this.article?.slug || ''
+    }
+  },
+  methods: {
+    async createComment () {
+      this.loading = true
+      try {
+        await articlesSlugCommentsCreateApi(this.slug, this.body)
+        const { comments } = await articlesSlugCommentsApi(this.slug)
+        this.comments = comments
+      } catch (err) {}
+      this.body = ''
+      this.loading = false
+    },
+    async deleteComment (id: string) {
+      if (this.loading) return
+      this.loading = true
+      await articlesSlugCommentsDeleteApi(this.slug, id)
+      const { comments } = await articlesSlugCommentsApi(this.slug)
+      this.comments = comments
+      this.loading = false
+    },
+    changeFollowing (value: boolean) {
+      if (this.article) this.article.author.following = value
+    },
+    changeArticle (value: Article) {
+      this.article = value
+    }
+  }
+})
 </script>
 
 <style scoped>
